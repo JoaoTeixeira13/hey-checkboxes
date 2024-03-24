763c14096bc21c19d9b1be0a2e54cf2eca8fc5d5
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2024-03-24 21:45:04 +0100

    refactor / cleanup

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const nativeCheckbox = document.querySelector("#native-input");
 const customCheckbox = document.querySelector("#custom-input");
-
 const toggleButton = document.querySelector("#toggle-button");
 
 nativeCheckbox.addEventListener("change", () => {
